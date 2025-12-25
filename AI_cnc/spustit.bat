1050157d4e929dc86661547722f7b06124923385
sstistkoo <stistko@gmail.com>
2025-12-25 11:26:22 +0100

    Smazání všech souborů z repozitáře

--- AI_cnc/spustit.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo.
-echo ============================================
-echo   CNC AI Programator - Spousteni
-echo ============================================
-echo.
-node serve.js
-pause
