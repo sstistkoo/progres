a67238e62e27d281d389d4c0453fb4e328d989eb
stistko <65913642+sstistkoo@users.noreply.github.com>
2025-12-31 23:46:53 +0100

    gg

--- programovani/js/scripts.js
+++ programovani/js/scripts.js
@@ -1,3 +1,9 @@
+// Vrátí aktuální kód z hlavního textarea editoru
+function getCurrentCode() {
+	var el = document.getElementById('codeInput');
+	if (el) return el.value;
+	return '';
+}
 // Přesunuto z html_studio.html
 
 // ============================================
